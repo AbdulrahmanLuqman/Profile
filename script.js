aa6343dfd74f48413ba2eeb48ed2dc920cf3ae31
AbdulrahmanLuqman <fal528@dervac.com>
2023-09-09 09:17:29 +0100

    made some corrections

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ const currentDate = new Date()
 const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 let currentDay = daysOfWeek[currentDate.getDay()]
-let currentUTCTime = currentDate.toISOString().substr(11, 8)
-console.log(currentUTCTime)
 let currentUTCHours = currentDate.getUTCHours()
 let currentUTCMInutes = currentDate.getUTCMinutes()
 let currentUTCSeconds = currentDate.getUTCSeconds()
