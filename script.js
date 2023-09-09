9d237fb57ebb80f5044523c537abdca55848f075
AbdulrahmanLuqman <fal528@dervac.com>
2023-09-09 11:32:28 +0100

    made some corrections

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-const currentDate = new Date()
-const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-let currentDay = daysOfWeek[currentDate.getDay()]
-let currentUTCHours = currentDate.getUTCHours()
-let currentUTCMInutes = currentDate.getUTCMinutes()
-let currentUTCSeconds = currentDate.getUTCSeconds()
-let currentUTCMilliseconds = currentDate.getUTCMilliseconds()
 
 
-document.querySelector('.currentDayOfTheWeek').innerHTML = currentDay
-document.querySelector('.currentUTCHours').innerHTML = currentUTCHours + 1
-document.querySelector('.currentUTCMinutes').innerHTML = currentUTCMInutes
-document.querySelector('.currentUTCSeconds').innerHTML = currentUTCSeconds
-document.querySelector('.currentUTCMilliseconds').innerHTML = currentUTCMilliseconds
+function today() {
+    const currentDate = new Date()
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const currentDay = daysOfWeek[currentDate.getDay()]
+    const currentUTCHours = currentDate.getUTCHours()
+    const currentUTCMInutes = currentDate.getUTCMinutes()
+    const currentUTCSeconds = currentDate.getUTCSeconds()
+    const currentUTCMilliseconds = currentDate.getUTCMilliseconds()
+    
 
+   document.querySelector('.currentDayOfTheWeek').innerHTML = currentDay
+   document.querySelector('.currentUTCHours').innerHTML = currentUTCHours + 1
+   document.querySelector('.currentUTCMinutes').innerHTML = currentUTCMInutes
+   document.querySelector('.currentUTCSeconds').innerHTML = currentUTCSeconds
+   document.querySelector('.currentUTCMilliseconds').innerHTML = currentUTCMilliseconds
+}
+
+today()
+setInterval(today, 1000);
