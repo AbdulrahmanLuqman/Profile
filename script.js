67396e99bf75daf96a444dae4d240bbca910ec40
AbdulrahmanLuqman <fal528@dervac.com>
2023-09-09 08:50:26 +0100

    made some corrections

--- script.js
+++ script.js
@@ -3,7 +3,16 @@ const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Fri
 
 let currentDay = daysOfWeek[currentDate.getDay()]
 let currentUTCTime = currentDate.toISOString().substr(11, 8)
+console.log(currentUTCTime)
+let currentUTCHours = currentDate.getUTCHours()
+let currentUTCMInutes = currentDate.getUTCMinutes()
+let currentUTCSeconds = currentDate.getUTCSeconds()
+let currentUTCMilliseconds = currentDate.getUTCMilliseconds()
 
 
 document.querySelector('.currentDayOfTheWeek').innerHTML = currentDay
-document.querySelector('.currentUTCTime').innerHTML = currentUTCTime
+document.querySelector('.currentUTCHours').innerHTML = currentUTCHours + 1
+document.querySelector('.currentUTCMinutes').innerHTML = currentUTCMInutes
+document.querySelector('.currentUTCSeconds').innerHTML = currentUTCSeconds
+document.querySelector('.currentUTCMilliseconds').innerHTML = currentUTCMilliseconds
+
